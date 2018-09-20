fb756b4ae7db332fb265e2ebba3dd98e81a7be52
Leonardo Matos <leomp120894@gmail.com>
2018-09-20 14:10:15 -0300

    minor code fix

--- cassandra_csv.sh
+++ cassandra_csv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$4" = "import" ]; then
- echo "COPY "$1"."$2" FROM '"$3"' WITH HEADER = TRUE ;"| cqlsh
+  echo "COPY "$1"."$2" FROM '"$3"' WITH HEADER = TRUE ;" | cqlsh
 else
- echo "COPY "$1"."$2" TO '"$3"' WITH HEADER = TRUE ;" | cqlsh
+  echo "COPY "$1"."$2" TO '"$3"' WITH HEADER = TRUE ;" | cqlsh
 fi
