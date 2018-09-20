4ecbbc6bc0a053045f97bbf58c4a482393408f1f
Leonardo Matos <leomp120894@gmail.com>
2018-09-20 18:21:27 -0300

    minor comments fixes

--- cqlcsv.sh
+++ cqlcsv.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 if [ "$4" = "import" ]; then
-  # cassandra -> csv
+  # csv -> cassandra
   echo "COPY "$1"."$2" FROM '"$3"' WITH HEADER = TRUE ;" | cqlsh
 else
-  # csv -> cassandra
+  # default
+  # cassandra -> csv
   echo "COPY "$1"."$2" TO '"$3"' WITH HEADER = TRUE ;" | cqlsh
 fi
