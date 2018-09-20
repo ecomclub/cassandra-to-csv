d02250b88caeea7c058f627791818bec80bbebef
Leonardo Matos <leomp120894@gmail.com>
2018-09-20 18:20:48 -0300

    rename file, add some comments

--- cqlcsv.sh
+++ cqlcsv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ "$4" = "import" ]; then
+  # cassandra -> csv
   echo "COPY "$1"."$2" FROM '"$3"' WITH HEADER = TRUE ;" | cqlsh
 else
+  # csv -> cassandra
   echo "COPY "$1"."$2" TO '"$3"' WITH HEADER = TRUE ;" | cqlsh
 fi
